50f4c2bcec95053c0be4afccb6f0b1d906d018b0
Jonathan Miller <jonathan.d.mllr@gmail.com>
2022-03-24 22:34:53 -0400

    fixed scope issue with code and updated README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+# Password Generator
+
+## Site URL
+https://jdmill.github.io/big-old-genny/
+
+## Overview
+
+1. Created password generator.
+2. Uses prompts to generate password criteria.
+3. Checks to make sure that desired password length is between 8-128 characters
+4. Confirms that minimum requirements are met before generation.
+
+## Special Notes.
+
+The only issue encounter in the development of this solution of this problem was a scoping issue. I did not realize it before, but var is a global variable. After some trial and error I debugged and made a modification to the code.
+
--- script.js
+++ script.js
@@ -1,50 +1,146 @@
-//create an array for each of the character options and concat based on choices
-//+= add all character values to the var password
-var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
-var lowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
-var numbers = [0,1,2,3,4,5,6,7,8,9];
-var special = ['~','`','/','!,','@','#','$','%','^','&','*','(',')',"'",'_','+','-','=','[',']','{','}',';',':','"',',','.','<','>','?'];
+// Inits all character arrays
+var upperCase = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+var lowerCase = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var special = [
+  "~",
+  "`",
+  "/",
+  "!,",
+  "@",
+  "#",
+  "$",
+  "%",
+  "^",
+  "&",
+  "*",
+  "(",
+  ")",
+  "'",
+  "_",
+  "+",
+  "-",
+  "=",
+  "[",
+  "]",
+  "{",
+  "}",
+  ";",
+  ":",
+  '"',
+  ",",
+  ".",
+  "<",
+  ">",
+  "?",
+];
 
-
+//function that generates the password value
 function generatePassword() {
-    //window prompts and booleans
-    var capChars = window.confirm("Should the password contain Upper Case Letters?");
-    var lowChars = window.confirm("Should the password contain Lower Case Letters?");
-    var numbs = window.confirm("Should the password contain Numbers?");
-    var spec = window.confirm("Should the password contain Special Characters?");
-    var number = Number(window.prompt("How many characters should the password be? Must be larger than 8 and no more than 128"));
+  //window prompts and booleans
+  var capChars = window.confirm(
+    "Should the password contain Upper Case Letters?"
+  );
+  var lowChars = window.confirm(
+    "Should the password contain Lower Case Letters?"
+  );
+  var numChars = window.confirm("Should the password contain Numbers?");
+  var specChars = window.confirm("Should the password contain Special Characters?");
 
-    //list var init
-    var charList = [];
+  //turns the prompt input into a number
+  var number = Number(
+    window.prompt(
+      "How many characters should the password be? Must be larger than 8 and no more than 128"
+    )
+  );
 
-    //adds desired characters to the character array
-    if(capChars){
-      charList = charList.concat(upperCase);
-    }
-    if(lowChars){
-      charList = charList.concat(lowerCase);
-    }
-    if(numbs){
-      charList = charList.concat(numbers);
-    }
-    if(spec){
-      charList = charList.concat(special);
-    }
-    console.log(charList);
+  //charList init
+  var charList = [];
 
-    //checks to see that password conditions are met
-    if (number >= 8 && number <= 128 && (capChars || lowChars || numbs || spec)) {
-        password = "";
-      //creates random number and retrieves value from charList. iterates desired number of times to create password of desired length
-      for (var i = 0; i < number; i++){
-        //Random number init
-        var value = Math.floor(Math.random() * charList.length);
-        password += charList[value];
-      }
-    } else {
-      window.alert("At least one Character Type must be accepted and the password must be between 8 and 128 characters");
+  //adds desired characters to the character array
+  if (capChars) {
+    charList = charList.concat(upperCase);
+  }
+  if (lowChars) {
+    charList = charList.concat(lowerCase);
+  }
+  if (numChars) {
+    charList = charList.concat(numbers);
+  }
+  if (specChars) {
+    charList = charList.concat(special);
+  }
+
+  //checks to see that password conditions are met
+  if (number >= 8 && number <= 128 && (capChars || lowChars || numChars || specChars)) {
+    //creates random number and retrieves value from charList. iterates desired number of times to create password of desired length
+    var generated = "";
+    for (var i = 0; i < number; i++) {
+      //Random number init
+      var value = Math.floor(Math.random() * charList.length);
+      generated += charList[value];
     }
-    return password;
+  } else {
+    //if the password length is long enough or characters are not selected display this alert
+    window.alert(
+      "At least one Character Type must be accepted and the password must be between 8 and 128 characters"
+    );
+  }
+  return generated;
 }
 
 // Assignment Code
@@ -56,7 +152,6 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
